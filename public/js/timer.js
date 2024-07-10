78c73c04e97656b9df1f8840759d8d972f8eedde
Esra Akay <esra.akay67@icloud.com>
2024-07-10 06:14:13 +0200

    Adjust timer logic to switch between work and break times

--- public/js/timer.js
+++ public/js/timer.js
@@ -1,11 +1,24 @@
 const WORK_MIN = 1;
+const BREAK_MIN = 2;
+const LONG_BREAK_MIN = 3; 
 
 const btn = document.querySelector(".timer-btn button");
 const timerOutput = document.querySelector(".timer-output");
+let counter = 1;
 
 
 const startTimer = async function() {
-    let time = minToSec(WORK_MIN);
+    if (counter % 8 == 0) {
+        updateTimer(LONG_BREAK_MIN); 
+    } else if (counter % 2 == 0) {
+        updateTimer(BREAK_MIN); 
+    } else if (counter % 2 == 1) {
+        updateTimer(WORK_MIN);
+    }
+}
+
+const updateTimer = function(minutes) {
+    let time = minToSec(minutes);
     let intervalId = setInterval(()=> {
         if (time >= 0) {
             let leftMinutes = getMinutes(time);
@@ -13,8 +26,9 @@ const startTimer = async function() {
             timerOutput.innerHTML = `${leftMinutes.toString().length == 1 ? "0" : ""}${leftMinutes}:${leftSeconds.toString().length == 1 ? "0" : ""}${leftSeconds}`;
             time -=1; 
         } else {
-            console.log("finisehed")
             clearInterval(intervalId);
+            counter += 1;
+            startTimer();
         }
     }, 1000)
 }
